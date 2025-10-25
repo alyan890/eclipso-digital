83ff00e4f5b7227bbb64b793d1e59a7c91461d8d
Alyan Aamir <alyanamir890@gmail.com>
2025-10-25 16:50:54 +0500

    T hathadya tha

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = ({ theme, setTheme }) => {
           href="#contact-us"
           className="text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-105 transition-all shadow-md"
         >
-          Connect
+          Connec
           <img src={assets.arrow_icon} width={14} alt="Arrow" />
         </a>
       </div>
